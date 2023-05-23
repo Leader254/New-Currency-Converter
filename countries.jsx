56fa3ece19dc9f97fa08c420d89b45770f98ebad
Leader254 <gatheru.samuel@students.kyu.ac.ke>
2023-05-23 14:26:35 +0300

    for collaboration

---
+++ countries.jsx
@@ -0,0 +1,16 @@
+const countries = [
+    [
+        'USD', // United States Dollar
+        'EUR', // Euro
+        'JPY', // Japanese Yen
+        'GBP', // British Pound Sterling
+        'CAD', // Canadian Dollar
+        'AUD', // Australian Dollar
+        'CHF', // Swiss Franc
+        'CNY', // Chinese Yuan
+        'SEK', // Swedish Krona
+        'NZD', // New Zealand Dollar
+      ]
+]
+
+export default countries
